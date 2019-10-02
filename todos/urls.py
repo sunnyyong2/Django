b4d3b3f43a21feb272659b4ed9406cc303fefc64
Sunny <sunnyyong2@gmail.com>
2019-10-02 15:36:06 +0900

    create 완성

--- todos/urls.py
+++ todos/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('new/', views.new),
+    path('create/', views.create),
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Todo
 
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def new(request):
+    return render(request, 'new.html')
+
+def create(request):
+    title = request.GET.get('title')
+    due_date = request.GET.get('duedate')
+
+    todo = Todo()
+    todo.title = title
+    todo.due_date = due_date
+    todo.save()
+
+    return redirect('/todos/')
